47a9c7b51ab4e8eb9e418c65fc44aad8ee81d237
maxim <pitikinm@gmail.com>
2026-02-15 18:25:12 +0300

    add Entity object user

---
+++ backend/src/main/java/Model/User.java
@@ -0,0 +1,53 @@
+package Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="login")
+    private String login;
+
+    @Column(name="password")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
