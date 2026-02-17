dc02509bb5d15d6f7598aff27b8293a14377bdae
maxim <pitikinm@gmail.com>
2026-02-17 17:27:57 +0300

    made more hard requirements to login

--- backend/src/main/java/models/User.java
+++ backend/src/main/java/models/User.java
@@ -24,6 +24,7 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(name="role",nullable = false)
     private UserRole role;
 
